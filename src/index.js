3df925f4ba9def15051cc487b67821f69fbc2e35
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-05-11 17:53:54 +0300

    import

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import './js/modals/authorization';
 import './js/partials/header';
 import './js/partials/main-content';
 import './js/partials/footer';
-
+import './js/partials/support.js';
 import './js/modals/about-book';
