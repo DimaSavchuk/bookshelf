1af08e595af78c7be9fb84e2d1ded5bde2e0a8fe
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-05-11 15:23:51 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/modals/authorization';
+
+import './js/partials/header';
+import './js/partials/main-content';
+import './js/partials/footer';
+
+import './js/modals/about-book';
---
+++ src/js/partials/main-content.js
@@ -0,0 +1,3 @@
+import './categories';
+import './support';
+import './bestsellers';
