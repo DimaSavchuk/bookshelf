6a7c2d92fa2e88e402038a89422a63aef3cfeb1a
nazario17 <hazarkorchak@gmail.com>
2023-05-13 17:10:58 +0300

    categories-list

---
+++ src/js/partials/categories.js
@@ -0,0 +1,26 @@
+const list = document.querySelector('.category-list');
+
+fetch('https://books-backend.p.goit.global/books/category-list')
+  .then(response => response.json())
+  .then(data => {
+    const arr = [];
+    for (const item of data) {
+      const values = item.list_name;
+      arr.push(values);
+    }
+
+    console.log(arr);
+
+    const markup = arr
+      .map(item => {
+        return `<li class="category-item">
+      <a class="category-link" href="#">${item}</a>
+      </li>`;
+      })
+      .join('');
+
+    list.insertAdjacentHTML('afterbegin', markup);
+  })
+  .catch(error => {
+    console.error('Сталася помилка при запиті:', error);
+  });
