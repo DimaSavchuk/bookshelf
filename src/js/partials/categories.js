467133989b9ffb428e8b61904a64c6ab10d6d47c
NGanch <86801593+NGanch@users.noreply.github.com>
2023-05-16 11:55:38 +0100

    comit category

--- src/js/partials/categories.js
+++ src/js/partials/categories.js
@@ -1,11 +1,28 @@
 import { listenerCount } from 'process';
 import { categoryRequest } from '../requests/apiRequests';
 import { apiInstance } from '../services/api';
-import { changeColorStyleInTitle, createAllCategoryList } from './bestsellers';
+import { topBooksRequest } from '../requests/apiRequests';
+import { changeColorStyleInTitle, createAllCategoryList, createTopBookCardList } from './bestsellers';
 
 const list = document.querySelector('.category-list');
 const categoryTitle = document.querySelector('.bestsellers-title');
 const bestSellersGalery = document.querySelector('.bestsellers');
+
+//================================================================
+import Notiflix from 'notiflix';
+
+// export async function topBooksRequest() {
+
+//   const response = await fetch(`https://books-backend.p.goit.global/books/top-books`);
+
+// if (!response.ok) {
+//   throw new Error(
+// console.log('Oops, there is no country with that name'));
+// }
+// return await response.json();
+// }
+
+//============================================================================================
 
 export function category() {
   fetch('https://books-backend.p.goit.global/books/category-list')
@@ -17,8 +34,6 @@ export function category() {
         arr.push(values);
       }
 
-      console.log(arr);
-
       const markup = arr
         .map(item => {
           return `<li class="category-item">
@@ -29,6 +44,7 @@ export function category() {
 
       list.insertAdjacentHTML('beforeend', markup);
       categoryListener();
+     
     })
     .catch(error => {
       console.error('Сталася помилка при запиті:', error);
@@ -39,17 +55,83 @@ function categoryListener() {
   list.addEventListener('click', handleCategoryClick);
 }
 
+// function handleCategoryClick(event) {
+//   event.preventDefault();
+//   const target = event.target;
+//   console.log(target)
+//   if (target.classList.contains('category-link')) {
+//     const categoryName = target.textContent;
+//     console.log(categoryName)
+
+//     if (target.textContent === 'All categories') {
+//       categoryTitle.innerHTML = `Best Sellers Books`;
+//       loadTopBooksOnClick();
+//     } 
+  
+//     else {
+//       categoryTitle.innerHTML = `${changeColorStyleInTitle(categoryName)}`;
+//       try {
+//         categoryRequest(categoryName)
+//           .then(data => {
+//             if (data.length === 0 || data === undefined) {
+//               Notiflix.Notify.failure(
+//                 "Sorry, we didn't find anything according to your request."
+//               );
+//               return;
+//             }
+
+//             bestSellersGalery.innerHTML = '';
+//             createAllCategoryList(data, categoryName);
+//           })
+//           .catch(err => console.log(err));
+//       } catch (error) {
+//         console.log(error.message);
+//       }
+//     }
+//   }
+// }
+
+//==============================
+// =======================================================
+
+
+const categoryAll = document.getElementById('category-all');
+
+categoryAll.addEventListener('click', onTitleClick)
+
+function onTitleClick (){
+  
+  try {
+    topBooksRequest()
+      .then(data => {
+        if (data.length === 0 || data === undefined) {
+          Notiflix.Notify.failure(
+            "Sorry, we didn't find anything according to your request."
+          );
+          return;
+        }
+        // bestSellersGalery.innerHTML = '';
+      
+        bestSellersGalery.innerHTML = ` <h1 class="bestsellers-title bestsellers-title-color">
+        Best Sellers
+        <span class="bestsellers-title bestsellers-title-span">Books</span>`;
+        categoryTitle.textContent = `Best Sellers Books`;
+        bestSellersGalery.insertAdjacentHTML(
+          'beforeend',
+          createTopBookCardList(data)
+        );
+      })
+      .catch(err => console.log(err));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+// ========================================================
 function handleCategoryClick(event) {
   event.preventDefault();
   const target = event.target;
-
   if (target.classList.contains('category-link')) {
-    const categoryName = target.textContent;
-
-    if (target.textContent === 'All category') {
-      categoryTitle.innerHTML = `Best Sellers Books`;
-      loadTopBooksOnClick();
-    } else {
+    const categoryName = target.textContent;  
       categoryTitle.innerHTML = `${changeColorStyleInTitle(categoryName)}`;
       try {
         categoryRequest(categoryName)
@@ -68,6 +150,6 @@ function handleCategoryClick(event) {
       } catch (error) {
         console.log(error.message);
       }
-    }
+    
   }
 }
